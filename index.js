06b4cbe3ba770d7c0468a335f39ceb5a37483ee0
Esbon Omwenga <victoromwenga365@gmail.com>
2022-12-09 11:52:05 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
-let distance = 0;
-function distanceFromHqInBlocks(block) {
-distance= Math.abs(42-block);
-return distance;
+// Code your solution in this file!
+
+const blocksFromHq = 42;
+let num1;
+let num2;
+let amountPayable;
+
+//THE DISTANCE IN BLOCKS.
+function distanceFromHqInBlocks (valueInBlocks) {
+    num1= Math.abs(valueInBlocks - blocksFromHq);
+        return num1;
 }
+distanceFromHqInBlocks(43);
+console.log(num1);
 
+//THE DISTANCE IN FEET.
+function distanceFromHqInFeet(valueInBlocks) {
+    distanceFromHqInBlocks(valueInBlocks)
+    num2 = num1*264;
+    return num2;
+}
+distanceFromHqInFeet(50);
+console.log(num2);
 
+//CALCULATE DISTANCE TRAVELLED IN FEET.
+function distanceTravelledInFeet(start,destination) {
+    let num3 = Math.abs (destination-start)* 264;
+    console.log(num3);
+    return num3;
+}
+distanceTravelledInFeet(42,45);
 
-function distanceFromHqInFeet(distance) {
-    let d= distanceFromHqInBlocks(distance); 
-
-    let feet= 0;
-    feet= d*264;
-    return feet;
-
+//CALCULATE FARE PRICES.
+function calculatesFarePrice(start, destination)
+ {let num4= Math.abs(((destination - start)*264));
+    if (num4 <= 400) {
+        return 0;
+      } else 
+            if (num4 > 400 && num4 < 2000) {
+         amountPayable = ((num4 - 400) * 0.02);
+        return amountPayable;
+      } 
+      else 
+            if(num4>=2000 && num4<2500){
+        return 25;
+      }
+      else 
+            if (num4 > 2500) {
+        return 'cannot travel that far';
+      }
     }
-
-
-    function distanceTravelledInFeet(start, destination) {
-        if (start< destination) {
-         return (destination-start)*264
-        }else{
-         return(start -destination)*264
-        }
-    }
-
-    
-    
-    function calculatesFarePrice(start, destination) {
-        let distanceBlock= Math.abs(start- destination)*264;
-        let fare = 0;
-        let foot= distanceBlock-400;
-
-        if (distanceBlock<=400){
-         return fare=0;   
-        } else if(distanceBlock > 400 && distanceBlock<=2000){
-         return fare=foot*0.02;
-        }else if ( distanceBlock > 2000 && distanceBlock < 2500){
-        return fare= 25;
-        }else {
-        return fare = "cannot travel that far";
-        }
-
-    }
+    calculatesFarePrice(44, 47);
+    console.log(amountPayable);
